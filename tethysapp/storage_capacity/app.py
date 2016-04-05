70aeac8dcfc25643fad63e1dcb7208f03805c312
michaels <masouffront@gmail.com>
2016-04-04 19:18:28 -0600

    First commit

---
+++ tethysapp/storage_capacity/app.py
@@ -0,0 +1,30 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class StorageCapacity(TethysAppBase):
+    """
+    Tethys app class for Storage Capacity.
+    """
+
+    name = 'Storage Capacity'
+    index = 'storage_capacity:home'
+    icon = 'storage_capacity/images/icon.gif'
+    package = 'storage_capacity'
+    root_url = 'storage-capacity'
+    color = 'blue'
+    enable_feedback = False
+    feedback_emails = []
+
+        
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (UrlMap(name='home',
+                           url='storage-capacity',
+                           controller='storage_capacity.controllers.home'),
+        )
+
+        return url_maps
